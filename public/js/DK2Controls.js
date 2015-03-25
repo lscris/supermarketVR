eeaf9f22dc9af0ac072740d2d19fe2abc34390b1
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2015-03-25 10:52:58 +0100

    test

--- public/js/DK2Controls.js
+++ public/js/DK2Controls.js
@@ -65,38 +65,41 @@ THREE.DK2Controls = function(camera) {
   
   
   this.onKeyDown = function (event) {
-    switch (event.keyCode) {
+
+    if(!pause) {
       
-      case 87: /*W*/
-        this.wasd.up = true;
-        this.moveForward = true;
-        break;
-
-      case 83: /*S*/
-        this.wasd.down = true;
-        this.moveBackward = true;
-        break;
-
-      case 68: /*D*/
-        this.wasd.right = true;
-        break;
-
-      case 65: /*A*/
-          this.wasd.left = true;
+      switch (event.keyCode) {
+        
+        case 87: /*W*/
+          this.wasd.up = true;
+          this.moveForward = true;
           break;
 
-      case 82: /*R*/
-          this.moveUp = true; 
+        case 83: /*S*/
+          this.wasd.down = true;
+          this.moveBackward = true;
           break;
 
-      case 70: /*F*/
-          this.moveDown = true;
+        case 68: /*D*/
+          this.wasd.right = true;
           break;
+
+        case 65: /*A*/
+            this.wasd.left = true;
+            break;
+
+        case 82: /*R*/
+            this.moveUp = true; 
+            break;
+
+        case 70: /*F*/
+            this.moveDown = true;
+            break;
+      }
 
     }
   };
   
-  // moveForward and moveBackward are used to move the pathCamera (see render method in index)
   this.onKeyUp = function (event) {
     switch (event.keyCode) {
 
@@ -143,7 +146,7 @@ THREE.DK2Controls = function(camera) {
 
       this.lastId = id;
     }
-      
+
     // update position
     if (this.wasd.up)
       this.controller.translateZ(-this.translationSpeed * delta);
@@ -162,10 +165,7 @@ THREE.DK2Controls = function(camera) {
     if (this.moveDown)
       this.controller.translateY( - this.translationSpeed * delta );
     
-    // update the position of the camera ONLY if pathEnabled == false. Otherwise the camare will follow the path.
-    if(!pathEnabled) {
-      this.camera.position.addVectors(this.controller.position, this.headPos);
-    }
+    this.camera.position.addVectors(this.controller.position, this.headPos);
 
     if (this.camera.position.y < -10) {
         this.camera.position.y = -10;
